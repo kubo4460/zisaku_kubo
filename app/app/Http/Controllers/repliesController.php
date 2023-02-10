5977ef9ba01ce7a83338b0798456fa1074d086ec
ジブ・ラルタル <ziburarutaru@jiburarutarunoMacBook-Air.local>
2023-02-10 23:54:26 +0900

    make main

---
+++ app/app/Http/Controllers/repliesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\replies;
+use Illuminate\Http\Request;
+
+class repliesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\replies  $replies
+     * @return \Illuminate\Http\Response
+     */
+    public function show(replies $replies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\replies  $replies
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(replies $replies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\replies  $replies
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, replies $replies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\replies  $replies
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(replies $replies)
+    {
+        //
+    }
+}
